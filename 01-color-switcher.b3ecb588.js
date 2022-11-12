bbc2bcd8cfbe35f1f35ca2cc63bbb7b379cee8e5
KamilSzafranski <107311803+KamilSzafranski@users.noreply.github.com>
2022-11-12 12:47:02 +0000

    Deploying to gh-pages from @ KamilSzafranski/goit-js-hw-09@355759200eec880057a29fd716cd0cd77d322508 🚀

---
+++ 01-color-switcher.b3ecb588.js
@@ -0,0 +1,2 @@
+const e=()=>`#${Math.floor(16777215*Math.random()).toString(16)}`,t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(l=>{const a=e();document.body.style.backgroundColor=a,d=setInterval((()=>{let t=e();for(;t===a;)console.log("Color Repeated!!!!"),t=e();document.body.style.backgroundColor=t}),1e3),t.disabled=!0,o.disabled=!1})),o.addEventListener("click",(e=>{clearInterval(d),t.disabled=!1,o.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.b3ecb588.js.map
